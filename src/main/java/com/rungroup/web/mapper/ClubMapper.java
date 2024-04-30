b1c79425bba20d9297854a7831d6782676f5ba53
Hüseyin Umut Reyhanoğlu <h.umutayran@gmail.com>
2024-04-30 15:51:16 +0300

    Created 'UserEntity' and 'Role' models

--- src/main/java/com/rungroup/web/mapper/ClubMapper.java
+++ src/main/java/com/rungroup/web/mapper/ClubMapper.java
@@ -3,6 +3,10 @@ package com.rungroup.web.mapper;
 import com.rungroup.web.dto.ClubDto;
 import com.rungroup.web.models.Club;
 
+import java.util.stream.Collectors;
+
+import static com.rungroup.web.mapper.EventMapper.mapToEventDto;
+
 public class ClubMapper {
     public static ClubDto mapToClubDto(Club club) {
         return ClubDto.builder()
@@ -12,6 +16,7 @@ public class ClubMapper {
                 .content(club.getContent())
                 .createdOn(club.getCreatedOn())
                 .updatedOn(club.getUpdatedOn())
+                .events(club.getEvents().stream().map(event -> mapToEventDto(event)).collect(Collectors.toList()))
                 .build();
     }
 
